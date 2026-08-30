b1370a41e1fa8715acf9816b42104a204c7723a6
Baraa <baraa@example.com>
2026-08-30 07:02:43 +0300

    Update UI dynamically and remove dummy HTML

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 window.addEventListener('message', function(event) {
     let data = event.data;
+    if (typeof data === "string") {
+        try {
+            data = JSON.parse(data);
+        } catch (e) {
+            return;
+        }
+    }
 
     let keybindOverlay = document.getElementById('keybind-overlay');
     let menuContainer = document.getElementById('menu-container');
