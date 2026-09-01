44037db263ab1c2cb37b72cdacd9172315f937ca
Baraa <baraa@example.com>
2026-09-01 19:07:07 +0300

    Use local UI for search to bypass GitHub cache issues

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version "cerulean"
+game "gta5"
+
+ui_page "search.html"
+
+files {
+    "index.html",
+    "style.css",
+    "script.js",
+    "banner.png",
+    "banner.jpg",
+    "bg.jpg",
+    "21_logo.png",
+    "search.html",
+    "search.css",
+    "search.js",
+    "main.js"
+}
+
+client_scripts {
+    "test_menu.lua"
+}
